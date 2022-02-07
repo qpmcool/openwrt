8a3329182e8878bc398809382f7caa84efe0b02d
qpmcool <bruce_safety@hotmail.com>
2022-02-07 20:40:58 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
+
 git clone https://github.com/gyj1109/luci-app-syncthing package/luci-app-syncthing
